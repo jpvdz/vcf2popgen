8864c2f95b713318010f825bc23d865dfe209683
Jurjan van der Zee <3024949+jpvdz@users.noreply.github.com>
2023-04-20 12:22:56 +0200

    Convert test code to proper unit test compatible with pytest

--- tests/vcf2popgen/test_utils.py
+++ tests/vcf2popgen/test_utils.py
@@ -1,14 +1,12 @@
 import pandas as pd
 
-def test_create_pop_dict(sample_map):
+def test_create_pop_dict():
+    sample_map = pd.Series(['C', 'A', 'A', 'B'])
+    expected_pop_dict = {'C' : 1, 'A': 2, 'B' : 3}
     pop_dict = {}
 
     for pop_id, pop_label in enumerate(sample_map.unique()):
         pop_dict[pop_label] = pop_id + 1
 
-    print(pop_dict)
+    assert pop_dict == expected_pop_dict
     
-if __name__ == '__main__':
-    test_sample_map = pd.Series(['D', 'A', 'A', 'A', 'B', 'B', 'C', 'C'])
-    print(test_sample_map)
-    test_create_pop_dict(test_sample_map)
